6465f1c58f3dd1c2b213c370199b7087ecc31839
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-04-12 20:36:42 +0000

    added home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -7,6 +7,28 @@ class ApplicationController < ActionController::Base
   def play_rock
 
     render({:template => "game_templates/user_rock.html.erb" })
+
+  end
+
+  def homepage
+
+    render({:template => "game_templates/rules.html.erb" })
+
+  end
+
+  def play_paper
+    
+    @comp_move = ["rock", "paper", "scissors"].sample 
+
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+     else 
+      @outcome = "tied"
+    end 
+    
+    render({:template => "game_templates/user_paper.html.erb" })
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/rock", {:controller => "application", :action => "play_rock"})
-
+  get("/", {:controller => "application", :action => "homepage"})
+  get("/paper", {:controller => "application", :action => "play_paper"})
 
 end
